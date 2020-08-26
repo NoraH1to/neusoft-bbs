7d59698e1c85dd9ad0fad76317ffb58f426e3445
yokaimeow <yokaimeow@gmail.com>
2020-08-25 22:43:13 -0400

    [#4] 样式优化

--- src/component/Category/index.jsx
+++ src/component/Category/index.jsx
@@ -4,17 +4,24 @@ import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Board from './Board'
 
+// add
+import Divider from '@material-ui/core/Divider'
+import Chip from '@material-ui/core/Chip'
+
 export default (props) => {
     const [category, setCategory] = useState(props.category)
     return (
         <Grid container direction="column" justify="center" alignItems="center">
             {/* 分区名称 */}
-            <Grid item>
-                <Typography>分区标题{category.name}</Typography>
+            <Grid item xs>
+                <Typography gutterBottom variant="h4">
+                    分区标题{category.name}
+                </Typography>
             </Grid>
             {/* 描述 */}
             <Grid item>
-                <Typography>分区描述{category.description}</Typography>
+                <Typography color="textSecondary">分区描述{category.description}</Typography>
+                <Divider />
             </Grid>
             <Grid>
                 {category.boardList.map((board) => (
