2f2a8e964ea5e98dd3bbd7a24cc5c1fa261562bd
yokaimeow <yokaimeow@gmail.com>
2020-08-26 22:22:14 -0400

    添加card

--- src/component/Category/index.jsx
+++ src/component/Category/index.jsx
@@ -3,28 +3,37 @@ import React, { useState } from 'react'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
+// import Box from '@material-ui/core/Box'
+import Card from '@material-ui/core/Card'
 import Board from './Board'
 
 export default (props) => {
     const [category, setCategory] = useState(props.category)
     return (
-        <Grid container direction="column" justify="center" alignItems="center">
-            {/* 分区名称 */}
-            <Grid item xs>
-                <Typography gutterBottom variant="h4">
-                    分区标题{category.name}
-                </Typography>
+        <>
+            <Grid container direction="column" justify="center" alignItems="center">
+                {/* <Box border={1} borderRadius={16} borderColor="primary.main"> */}
+                <Card>
+                    <Grid item xs>
+                        <Typography gutterBottom variant="h4">
+                            分区标题{category.name}
+                        </Typography>
+                    </Grid>
+                    {/* 描述 */}
+                    <Grid item>
+                        <Typography color="textSecondary" variant="h5">
+                            分区描述{category.description}
+                        </Typography>
+                        <Divider />
+                    </Grid>
+                    <Grid>
+                        {category.boardList.map((board) => (
+                            <Board board={board} />
+                        ))}
+                    </Grid>
+                </Card>
+                {/* </Box> */}
             </Grid>
-            {/* 描述 */}
-            <Grid item>
-                <Typography color="textSecondary" variant="h5">分区描述{category.description}</Typography>
-                <Divider />
-            </Grid>
-            <Grid>
-                {category.boardList.map((board) => (
-                    <Board board={board} />
-                ))}
-            </Grid>
-        </Grid>
+        </>
     )
 }
